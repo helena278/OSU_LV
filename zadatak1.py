95a9f1e43c35018cf812ce170c8424f5beb03486
helena278 <helenaandrasek0@gmail.com>
2026-03-31 09:33:12 +0200

    Delete zadatak1.py

--- zadatak1.py
+++
@@ -1,9 +0,0 @@
-workHours = float(input("Unesite broj radnih sati:"))
-hourPrice = float(input("Unesite iznos po radnom satu"))
-
-
-
-def totalEuro(workHours,hourPrice):
-    return workHours*hourPrice
-
-print(f"Ukupno: {totalEuro(workHours,hourPrice)} eura")
